f61b1e2e4d8ec514c9c5c515a07bacb5da29dea8
Ancevt <me@ancevt.com>
2025-09-02 21:54:38 +0600

    BattleEngine per-second pattern

--- src/main/java/com/fellon/battle/BattleApplication.java
+++ src/main/java/com/fellon/battle/BattleApplication.java
@@ -5,6 +5,7 @@ import com.ancevt.d2d2.ApplicationContext;
 import com.ancevt.d2d2.D2D2;
 import com.ancevt.d2d2.debug.FpsMeter;
 import com.ancevt.d2d2.lifecycle.Application;
+import com.ancevt.d2d2.log.Log;
 
 public class BattleApplication implements Application {
     public static void main(String[] args) {
@@ -24,8 +25,16 @@ public class BattleApplication implements Application {
         );
     }
 
+    private BattleEngine battleEngine;
+
+
     @Override
     public void start(ApplicationContext context) {
+        D2D2.log.setLevel(Log.DEBUG);
+
+        battleEngine = new BattleEngine();
+        battleEngine.start();
+
 
         context.stage().addChild(new FpsMeter());
     }
