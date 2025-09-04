a779bfb92cc5b40aa542be2e89760f201849cfe1
fellon-dnb <fellon@ancevt.com>
2025-09-04 17:50:08 +0300

    add to Battle class even Listeners, inner methods

--- src/main/java/com/fellon/battle/model/Battle.java
+++ src/main/java/com/fellon/battle/model/Battle.java
@@ -1,12 +1,51 @@
 package com.fellon.battle.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Battle {
     private State state;
     private Hero hero;
     private Monster monster;
-public Battle(Hero hero, Monster monster) {
-    this.hero = hero;
-    this.monster = monster;
-    this.state = State.NON_STARTED;
+
+    public Battle(Hero hero, Monster monster) {
+        this.hero = hero;
+        this.monster = monster;
+        this.state = State.NON_STARTED;
+    }
+
+    private List<BattleListener> listeners = new ArrayList<>();
+
+    public void addListener(BattleListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(BattleListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void fireBattleStart() {
+        for (BattleListener listener : listeners) {
+            listener.onBattleStarted(this);
+        }
+    }
+
+    private void fireTurnStart(HasCombatStats attacker, HasCombatStats defender, int turn) {
+        for (BattleListener listener : listeners) {
+            listener.onTurnStarted(this, attacker, defender, turn);
+        }
+    }
+
+    private void fireTurnEnd(HasCombatStats attacker, HasCombatStats defender, int turn) {
+        for (BattleListener listener : listeners) {
+            listener.onTurnEnded(this, attacker, defender, turn);
+        }
+    }
+    private void fireBattleEnd(HasCombatStats winner, HasCombatStats loser) {
+        for (BattleListener listener : listeners) {
+            listener.onBatttleEnded(this, winner, loser);
+        }
+    }
 }
-}
+
--- src/main/java/com/fellon/battle/model/BattleListener.java
+++ src/main/java/com/fellon/battle/model/BattleListener.java
@@ -1,9 +1,16 @@
 package com.fellon.battle.model;
 
 public interface BattleListener {
-default void onBattleStarted(Battle battle) {}
-    default void onTurnStarted(Battle battle, HasCombatStats attacker, HasCombatStats deffender, int turnNumber) {}
-default  void onTurnEnded(Battle battle, HasCombatStats attacker, HasCombatStats deffender, int turnNumber) {}
-default void onBatttleEnded(Battle battle, HasCombatStats winner, HasCombatStats loser) {}
+    default void onBattleStarted(Battle battle) {
+    }
+
+    default void onTurnStarted(Battle battle, HasCombatStats attacker, HasCombatStats deffender, int turnNumber) {
+    }
+
+    default void onTurnEnded(Battle battle, HasCombatStats attacker, HasCombatStats deffender, int turnNumber) {
+    }
+
+    default void onBatttleEnded(Battle battle, HasCombatStats winner, HasCombatStats loser) {
+    }
 }
 
